542acf0dd18612001aa15653e70ba4ece141dff6
REM-Codes <rowan.mcalpin@gmail.com>
2023-09-26 08:14:01 -0700

    Additional fixes and testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectoryFactory/CompetitionTrajectoryFactory.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectoryFactory/CompetitionTrajectoryFactory.kt
@@ -1,9 +1,27 @@
 package org.firstinspires.ftc.teamcode.trajectoryFactory
 
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import org.atomicrobotics3805.cflib.trajectories.ParallelTrajectory
 import org.atomicrobotics3805.cflib.trajectories.TrajectoryFactory
+import org.atomicrobotics3805.cflib.trajectories.rad
+import org.atomicrobotics3805.cflib.trajectories.switch
+import org.atomicrobotics3805.cflib.trajectories.switchAngle
+import org.atomicrobotics3805.cflib.Constants.drive as d
 
 object CompetitionTrajectoryFactory : TrajectoryFactory() {
+    var startPoseA = Pose2d()
+    var poseB = Pose2d()
+
+    lateinit var startAtoPoseB : ParallelTrajectory
+
     override fun initialize() {
         super.initialize()
+
+        startPoseA = Pose2d(35.0, 62.75.switch, 270.0.switchAngle.rad)
+        poseB = Pose2d(60.75, 12.15.switch, 0.0.switchAngle.rad)
+
+        startAtoPoseB = d.trajectoryBuilder(startPoseA)
+            .splineToSplineHeading(poseB, 180.0.rad)
+            .build()
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visualization/Visualizer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visualization/Visualizer.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.visualization
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.noahbres.meepmeep.MeepMeep
 import org.atomicrobotics3805.cflib.Constants
 import org.atomicrobotics3805.cflib.driving.drivers.MecanumDrive
 import org.atomicrobotics3805.cflib.driving.localizers.TwoWheelOdometryLocalizer
@@ -17,16 +18,18 @@ fun main() {
             MecanumDrive(
                 PracticeMecanumDriveConstants,
                 TwoWheelOdometryLocalizer(PracticeOdometryConstants())
+
             ) { Pose2d(0.0, 0.0, 0.0) },
             14.5,
             15.0,
             {
                 sequential {
-
+                    +Constants.drive.followTrajectory(CompetitionTrajectoryFactory.startAtoPoseB)
                 }
             },
             Constants.Side.LEFT
         )
     )
-    MeepMeepVisualizer.run(CompetitionTrajectoryFactory)
+
+    MeepMeepVisualizer.run(CompetitionTrajectoryFactory, background = MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
 }
