530dc204a6c14256b63a3fa3a7d480ac3e2f6a5d
rowan_mcalpin <rowan.mcalpin@gmail.com>
2023-10-05 16:03:04 -0700

    Update to CFLib 2.0.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestControls.kt
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode
-
-import org.atomicrobotics3805.cflib.controls.Controls
-
-object TestControls : Controls() {
-    override fun registerCommands() {
-
-    }
-}
