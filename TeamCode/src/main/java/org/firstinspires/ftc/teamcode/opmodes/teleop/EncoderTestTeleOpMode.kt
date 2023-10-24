32e994548eeac0488dfc0635f0565ee5619d71c7
AdamMEI <aimeilicke@gmail.com>
2023-10-24 10:55:04 -0700

    Test Encoder ticks directly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/EncoderTestTeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/EncoderTestTeleOpMode.kt
@@ -2,10 +2,15 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import org.atomicrobotics3805.cflib.Command
 import org.atomicrobotics3805.cflib.Constants
+import org.atomicrobotics3805.cflib.TelemetryController
 import org.atomicrobotics3805.cflib.driving.drivers.MecanumDrive
 import org.atomicrobotics3805.cflib.driving.localizers.TwoWheelOdometryLocalizer
 import org.atomicrobotics3805.cflib.opmodes.TeleOpMode
+import org.atomicrobotics3805.cflib.parallel
+import org.atomicrobotics3805.cflib.roadrunner.Encoder
 import org.atomicrobotics3805.cflib.trajectories.rad
 import org.atomicrobotics3805.cflib.utilCommands.TelemetryCommand
 import org.firstinspires.ftc.teamcode.drive.CompetitionDriveConstants
@@ -16,10 +21,26 @@ import org.firstinspires.ftc.teamcode.controls.CompetitionControls
 class EncoderTestTeleOpMode : TeleOpMode(
     CompetitionControls(),
     Constants.Color.RED,
-    mainRoutine = { TelemetryCommand(100.0) { Constants.drive.localizer.poseEstimate.toString() } },
+    mainRoutine = { parallel {
+        +TelemetryCommand(100.0) { Constants.drive.localizer.poseEstimate.toString() }
+        +DisplayEncoderCount()
+    } },
     drive = MecanumDrive(
         CompetitionDriveConstants,
         TwoWheelOdometryLocalizer(CompetitionOdometryConstants)
     ) { Pose2d(0.0, 0.0, 0.0.rad) },
     subsystems = arrayOf()
 )
+class DisplayEncoderCount : Command() {
+    private lateinit var encoder: Encoder
+    override val _isDone: Boolean
+        get() = false
+
+    override fun start() {
+        encoder = Encoder(Constants.opMode.hardwareMap.get(DcMotorEx::class.java, CompetitionOdometryConstants.PARALLEL_NAME))
+    }
+
+    override fun execute() {
+        TelemetryController.telemetry.addData("Encoder Count", encoder.currentPosition)
+    }
+}
